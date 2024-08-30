c513130eed827ce3768d2af784255947eb823f7a
jessymonroydev <jessymonroydev@gmail.com>
2024-08-30 02:19:18 -0500

    first

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'https://fundacionvolveranacerkids.org', // Permitir solo este dominio
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Si necesitas enviar cookies o encabezados de autorización
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
